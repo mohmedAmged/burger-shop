e361ac20dee86802b814eb9e41dde112d291acb7
Mohamed Amged <mohamedamged55555@gmail.com>
2025-11-08 20:22:50 +0200

    add autoplay for video when mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,145 +1,133 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/all";
-import { useRef } from "react";
+import { useRef} from "react";
 import { useMediaQuery } from "react-responsive";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Hero = () => {
-    const videoRef = useRef();
+ const videoRef = useRef();
 
-    const isMobile = useMediaQuery({ maxWidth: 767 });
+	const isMobile = useMediaQuery({ maxWidth: 767 });
 
-    useGSAP(() => {
-        const heroSplit = new SplitText(".title", {
-            type: "chars, words",
-        });
+	useGSAP(() => {
+		const heroSplit = new SplitText(".title", {
+			type: "chars, words",
+		});
 
-        const paragraphSplit = new SplitText(".subtitle", {
-            type: "lines",
-        });
+		const paragraphSplit = new SplitText(".subtitle", {
+			type: "lines",
+		});
 
-        // Apply text-gradient class once before animating
-        heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+		// Apply text-gradient class once before animating
+		heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
-        gsap.from(heroSplit.chars, {
-            yPercent: 100,
-            duration: 1.8,
-            ease: "expo.out",
-            stagger: 0.06,
-        });
+		gsap.from(heroSplit.chars, {
+			yPercent: 100,
+			duration: 1.8,
+			ease: "expo.out",
+			stagger: 0.06,
+		});
 
-        gsap.from(paragraphSplit.lines, {
-            opacity: 0,
-            yPercent: 100,
-            duration: 1.8,
-            ease: "expo.out",
-            stagger: 0.06,
-            delay: 1,
-        });
+		gsap.from(paragraphSplit.lines, {
+			opacity: 0,
+			yPercent: 100,
+			duration: 1.8,
+			ease: "expo.out",
+			stagger: 0.06,
+			delay: 1,
+		});
 
-        gsap
-            .timeline({
-                scrollTrigger: {
-                    trigger: "#hero",
-                    start: "top top",
-                    end: "bottom top",
-                    scrub: true,
-                },
-            })
-            .to(".right-leaf", { y: 200 }, 0)
-            .to(".left-leaf", { y: -200 }, 0)
-            .to(".arrow", { y: 100 }, 0);
+		gsap
+			.timeline({
+				scrollTrigger: {
+					trigger: "#hero",
+					start: "top top",
+					end: "bottom top",
+					scrub: true,
+				},
+			})
+			.to(".right-leaf", { y: 200 }, 0)
+			.to(".left-leaf", { y: -200 }, 0)
+			.to(".arrow", { y: 100 }, 0);
 
-        const startValue = isMobile ? "top 50%" : "center 60%";
-        const endValue = isMobile ? "120% top" : "bottom top";
+		const startValue = isMobile ? "top 50%" : "center 60%";
+		const endValue = isMobile ? "120% top" : "bottom top";
 
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: "video",
-                start: startValue,
-                end: endValue,
-                scrub: true,
-                pin: true,
-            },
-        });
-
-
-        videoRef.current.onloadedmetadata = () => {
-            tl.to(videoRef.current, {
-                currentTime: videoRef.current.duration,
-                duration: 3,
-                ease: "none",
-            });
-        };
-
-        if (videoRef.current) {
-            videoRef.current.play().catch(() => { });
-        }
-
-        videoRef.current.oncanplay = () => {
-            videoRef.current.play();
-
-            tl.to(videoRef.current, {
-                currentTime: videoRef.current.duration,
-                duration: 3,
-                ease: "none",
-            });
-        };
-    }, []);
+		let tl = gsap.timeline({
+		 scrollTrigger: {
+			trigger: "video",
+			start: startValue,
+			end: endValue,
+			scrub: true,
+			pin: true,
+		 },
+		});
 
 
-    return (
-        <>
-            <section id="hero" className="noisy">
-                <h1 className="title">Burgro</h1>
+		videoRef.current.onloadedmetadata = () => {
+		 tl.to(videoRef.current, {
+			currentTime: videoRef.current.duration,
+			duration: 3,
+			ease: "none",
+		 });
+		};
+	}, []);
 
-                <img
-                    src="/images/katchep2.png"
-                    alt="left-katchep"
-                    className="left-katchep katcheb_img_left lg:block opacity-[.8] w-50 md:w-80"
-                />
-                <img
-                    src="/images/katchep2.png"
-                    alt="right-katchep"
-                    className="right-katchep katcheb_img_right w-50 md:w-80 lg:block opacity-[.8]"
-                />
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">Burgro</h1>
 
-                <div className="body">
-                    <img src="/images/arrow.png" alt="arrow" className="arrow" />
+        <img
+          src="/images/katchep2.png"
+          alt="left-katchep"
+          className="left-katchep katcheb_img_left lg:block opacity-[.8] w-50 md:w-80"
+        />
+        <img
+          src="/images/katchep2.png"
+          alt="right-katchep"
+          className="right-katchep katcheb_img_right w-50 md:w-80 lg:block opacity-[.8]"
+        />
 
-                    <div className="content">
-                        <div className="space-y-5 hidden md:block">
-                            <p>Bold. Juicy. Legendary.</p>
-                            <p className="subtitle">
-                                Taste the Flame<br /> Love the Crunch
-                            </p>
-                        </div>
+        <div className="body">
+          <img src="/images/arrow.png" alt="arrow" className="arrow" />
 
-                        <div className="view-burgers">
-                            <p className="subtitle">
-                                Every burger we craft is packed with fire-grilled flavor,
-                                layered with melty cheese, crisp veggies,
-                                and sauces born to blow your mind.
-                            </p>
-                            <a href="#burgers">View Burgers</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <div className="video absolute inset-0">
-                <video
-                    ref={videoRef}
-                    muted
-                    autoPlay
-                    playsInline
-                    webkit-playsinline="true"
-                    preload="auto"
-                    src="/videos/output7.mp4"
-                />
-                {/* <div className="overlay absolute inset-0 bg-black/50" /> */}
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Bold. Juicy. Legendary.</p>
+              <p className="subtitle">
+                Taste the Flame<br /> Love the Crunch
+              </p>
             </div>
-        </>
-    );
+
+            <div className="view-burgers">
+              <p className="subtitle">
+                Every burger we craft is packed with fire-grilled flavor,
+                layered with melty cheese, crisp veggies,
+                and sauces born to blow your mind.
+              </p>
+              <a href="#burgers">View Burgers</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* ✅ Video wrapper that gets pinned */}
+        <div className="video absolute inset-0">
+				<video
+					ref={videoRef}
+					muted
+					playsInline
+                    autoPlay={isMobile? true: false}
+					preload="auto"
+					src="/videos/output7.mp4"
+				/>
+	    </div>
+    </>
+  );
 };
 
 export default Hero;
