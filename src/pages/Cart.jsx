a250e19485c7f02cad1d85dc2f85c86b5f5d3a6b
Mohamed Amged <mohamedamged55555@gmail.com>
2025-12-22 07:06:41 +0200

    `Updated Cart page with condition if no items in cart`

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,36 +3,36 @@ import { useCartStore } from '../store/cart.store'
 import InsideLoader from '../utils/InsideLoader';
 export default function Cart() {
     const { getCart, updateCartItem, removeFromCart, items, totalPrice, loading } = useCartStore();
-  
+
     const [quantities, setQuantities] = useState({})
 
     useEffect(() => {
-    if (items?.length) {
-        const initial = {}
-        items.forEach(item => {
-        initial[item.product.slug] = item.quantity
-        })
-        setQuantities(initial)
-    }
+        if (items?.length) {
+            const initial = {}
+            items.forEach(item => {
+                initial[item.product.slug] = item.quantity
+            })
+            setQuantities(initial)
+        }
     }, [items])
 
     const increment = (slug) => {
-    setQuantities(q => ({
-        ...q,
-        [slug]: q[slug] + 1
-    }))
+        setQuantities(q => ({
+            ...q,
+            [slug]: q[slug] + 1
+        }))
     }
     const decrement = (slug) => {
-    setQuantities(q => ({
-        ...q,
-        [slug]: Math.max(1, q[slug] - 1)
-    }))
+        setQuantities(q => ({
+            ...q,
+            [slug]: Math.max(1, q[slug] - 1)
+        }))
     }
 
-    const handleUpdate = (slug) =>{
+    const handleUpdate = (slug) => {
         updateCartItem(slug, quantities[slug])
     }
-    const handleRemove = (slug)=>{
+    const handleRemove = (slug) => {
         removeFromCart(slug)
     }
     console.log(items);
@@ -49,96 +49,113 @@ export default function Cart() {
                     <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
                         <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
                             <div className="space-y-6">
-                                {
-                                    loading ?
-                                        <div className='text-center flex-center '>
-                                            <InsideLoader />
+{
+    loading ?
+        <div className='text-center flex-center '>
+            <InsideLoader />
+        </div>
+        :
+        <>
+            {
+                items?.length !== 0 ?
+                    items?.map((item) => {
+                        const slug = item?.product?.slug
+                        const qty = quantities[slug] ?? item?.quantity
+                        return (
+                            <div
+                                key={item?._id}
+                                className="rounded-3xl border border-yellow/20 bg-black/50 p-6 md:p-6">
+                                <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                                    <a href="#" className="shrink-0 md:order-1">
+                                        <img className="h-20 w-20 dark:block" src={item?.product?.image} alt={item?.product?._id} />
+                                    </a>
+
+                                    <label htmlFor="counter-input" className="sr-only">Choose quantity:</label>
+                                    <div className="flex items-center justify-between md:order-3 md:justify-end">
+                                        <div className="flex items-center flex-wrap gap-4">
+                                            <button
+                                                onClick={() => decrement(slug)}
+                                                type="button"
+                                                id="decrement-button" data-input-counter-decrement="counter-input"
+                                                className="h-7 w-7">
+                                                ➖
+                                            </button>
+                                            <input
+                                                type="text"
+                                                id="counter-input"
+                                                data-input-counter className="" placeholder=""
+                                                value={qty}
+                                                readOnly
+                                            />
+
+                                            <button
+                                                onClick={() => increment(slug)}
+                                                type="button"
+                                                id="increment-button" data-input-counter-increment="counter-input"
+                                                className="h-7 w-7">
+                                                ➕
+                                            </button>
+
                                         </div>
-                                        :
-<>
-    {
-        
-            items?.map((item) => {
-            const slug = item?.product?.slug
-            const qty = quantities[slug] ?? item?.quantity
-                return (
-                <div 
-                key={item?._id}
-                className="rounded-3xl border border-yellow/20 bg-black/50 p-6 md:p-6">
-                    <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-                        <a href="#" className="shrink-0 md:order-1">
-                            <img className="h-20 w-20 dark:block" src={item?.product?.image} alt={item?.product?._id} />
-                        </a>
+                                        <div className="text-end md:order-4 md:w-36">
+                                            <p className="text-2xl font-modern-negra text-yellow price-tag">
+                                                {item?.itemTotal} {item?.product?.currency}
+                                            </p>
+                                            <p className="text-lg font-modern-negra text-white price-tag">
+                                                ({item?.product?.price} {item?.product?.currency} * {qty})
+                                            </p>
+                                        </div>
+                                    </div>
 
-                        <label htmlFor="counter-input" className="sr-only">Choose quantity:</label>
-                        <div className="flex items-center justify-between md:order-3 md:justify-end">
-                            <div className="flex items-center flex-wrap gap-4">
-                                <button
-                                    onClick={() => decrement(slug)}
-                                    type="button" 
-                                    id="decrement-button" data-input-counter-decrement="counter-input"
-                                    className="h-7 w-7">
-                                    ➖
-                                </button>
-                                <input
-                                    type="text"
-                                    id="counter-input" 
-                                    data-input-counter className="" placeholder=""
-                                    value={qty}
-                                    readOnly 
-                                    />
+                                    <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                                        <a href="#" className="text-xl font-modern-negra text-white hover:text-yellow">{item?.product?.name}<br /> {item?.product?.description}
+                                        </a>
 
-                                <button
-                                    onClick={() => increment(slug)}
-                                    type="button" 
-                                    id="increment-button" data-input-counter-increment="counter-input"
-                                    className="h-7 w-7">
-                                    ➕
-                                </button>
-                                
+                                        <div className="flex items-center gap-4 mt-4">
+
+                                            <button
+                                                onClick={() => handleRemove(slug)}
+                                                className="inline-flex items-center text-sm font-medium text-red-400 hover:text-red-300 cursor-pointer">
+                                                <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+                                                </svg>
+                                                Remove
+                                            </button>
+                                            <button
+                                                onClick={() => handleUpdate(slug)}
+                                                className="inline-flex items-center text-sm font-medium text-green-400 hover:text-green-300 cursor-pointer"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="me-1.5 h-5 w-5">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                                </svg>
+                                                Update
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="text-end md:order-4 md:w-36">
-                                <p className="text-2xl font-modern-negra text-yellow price-tag">
-                                    {item?.itemTotal} {item?.product?.currency}
-                                </p>
-                                <p className="text-lg font-modern-negra text-white price-tag">
-                                    ({item?.product?.price} {item?.product?.currency} * {qty})
-                                </p>
+                        )
+                    })
+                    :
+                    <>
+                        <div
+                            className="rounded-3xl border border-yellow/20 bg-black/50 p-6 md:p-6">
+                            <div className="space-y-4 md:flex md:items-center md:justify-center md:gap-6 md:space-y-0">
+                                <a href="/menu" title="" className="inline-flex items-center gap-2 text-sm font-medium text-yellow underline hover:no-underline">
+                                    No items here, Continue Shopping
+                                    <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
+                                    </svg>
+                                </a>
+
+
+
                             </div>
                         </div>
-
-                        <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-                            <a href="#" className="text-xl font-modern-negra text-white hover:text-yellow">{item?.product?.name}<br /> {item?.product?.description} 
-                            </a>
-
-                            <div className="flex items-center gap-4 mt-4">
-
-                                <button 
-                                onClick={() => handleRemove(slug)}
-                                className="inline-flex items-center text-sm font-medium text-red-400 hover:text-red-300 cursor-pointer">
-                                    <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
-                                    </svg>
-                                    Remove
-                                </button>
-                                <button
-                                    onClick={() => handleUpdate(slug)}
-                                    className="inline-flex items-center text-sm font-medium text-green-400 hover:text-green-300 cursor-pointer"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="me-1.5 h-5 w-5">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                                    </svg>
-                                    Update
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )})
-        
-    }
-</>
-                                }
+                    </>
+            }
+        </>
+}
 
                             </div>
 
@@ -181,7 +198,7 @@ export default function Cart() {
 
                                 <div className="flex items-center justify-center gap-2">
                                     <span className="text-sm font-normal text-white/70"> or </span>
-                                    <a href="#" title="" className="inline-flex items-center gap-2 text-sm font-medium text-yellow underline hover:no-underline">
+                                    <a href="/menu" title="" className="inline-flex items-center gap-2 text-sm font-medium text-yellow underline hover:no-underline">
                                         Continue Shopping
                                         <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
@@ -201,6 +218,10 @@ export default function Cart() {
                             </div>
                         </div>
                     </div>
+
+
+
+
                 </div>
             </section>
         </div>
