7c40ee10f6e55f190d57d02a2fa88ad08011f264
Mohamed Amged <mohamedamged55555@gmail.com>
2025-12-25 11:49:45 +0200

    `Added response interceptor to handle 401 status code and logout automatic and remove data from cookies`

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,4 +14,25 @@ api.interceptors.request.use((config) => {
   return config
 })
 
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove('token')
+      Cookies.remove('userData')
+      
+      const protectedPaths = ['/cart', '/checkout', '/all-orders']
+      const currentPath = window.location.pathname
+      
+      const isProtected = protectedPaths.some(path => currentPath.startsWith(path))
+      
+      if (isProtected) {
+        window.location.href = '/sign-in'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
